84c8bd266a5214ce565677d13caeceeb10c8c329
jj <implementaimplementa@gmail.com>
2024-04-12 20:14:47 -0600

    first commit

---
+++ resources/views/retiros/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.principal')
+
+@section('hijos')
+
+<h1>Modulo de Retiros Afiliado: {{$afiliado->id}} {{$afiliado->nombre}} {{$afiliado->apellido}}</h1>
+
+
+<table class="table">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>codempleado</th>
+                <th>codafiliado</th>
+                <th>fecha</th>
+                <th>monto</th>
+                <th>Acciones</th>
+            </tr>  
+        </thead>
+
+        <tbody>
+
+            @foreach($retiros as $retiro)
+            @if($retiro->codafiliado==$afiliado->id)
+            <tr>
+            <th>{{$retiro->id}}</th>
+                <th>{{$retiro->codempleado}}</th>
+                <th>{{$retiro->codafiliado}}</th>
+                <th>{{$retiro->fecha}}</th>
+                <th>{{$retiro->monto}}</th>
+                <th>
+                    
+                </th>
+              </tr>
+
+
+             @endif
+            @endforeach
+        </tbody>
+
+</table>
+
+
+
+@endsection
